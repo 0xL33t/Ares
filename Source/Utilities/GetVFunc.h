de1863f31d1c0c4d012ca233a71b701a75cdca16
aphu <aphu@420blaze.it>
2017-01-28 11:19:35 +0000

    Update GetVFunc.h

--- Source/Utilities/GetVFunc.h
+++ Source/Utilities/GetVFunc.h
@@ -1,8 +1,17 @@
 /* Ares-GetVFunc */
 #pragma once
 
+/* GetVTable */
+inline void**& GetVTable(void* inst, size_t offset = 0) {
+	return *reinterpret_cast<void***>((size_t)inst + offset);
+}
+
+inline const void** GetVTable(const void* inst, size_t offset = 0) {
+	return *reinterpret_cast<const void***>((size_t)inst + offset);
+}
+
 /* GetVFunc */
-template< typename Fn >
-inline Fn GetVFunc(const void* v, int i) {
-	return (Fn)*(*(const void***)v + i);
+template<typename Fn>
+inline Fn GetVFunc(const void* inst, size_t index, size_t offset = 0) {
+	return reinterpret_cast<Fn>(GetVTable(inst, offset)[index]);
 }
