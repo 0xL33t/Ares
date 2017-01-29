51c34336dfc596184394838d5c92413b9cfbde50
aphu <aphu@420blaze.it>
2017-01-28 20:08:24 -0500

    Update Pattern.cpp

--- Source/Utilities/Pattern/Pattern.cpp
+++ Source/Utilities/Pattern/Pattern.cpp
@@ -1,29 +1,6 @@
 /* Ares-Pattern */
 #include "../../Ares.h"
 
-/* GetProcessLibraries */
-std::unordered_map<const char*, uintptr_t> GetProcessLibraries() {
-	std::unordered_map<const char*, uintptr_t> modules;
-
-	dl_iterate_phdr([](struct dl_phdr_info* info, size_t size, void* data) {
-		reinterpret_cast<std::unordered_map<const char*, uintptr_t>*>(data)->insert({ info->dlpi_name, reinterpret_cast<uintptr_t>(info->dlpi_addr + info->dlpi_phdr[0].p_vaddr) });
-		return 0;
-	}, &modules);
-
-	return modules;
-}
-
-/* GetLibraryInformation */
-uintptr_t Pattern::GetLibraryAddress(const char* moduleName) {
-	std::unordered_map<const char*, uintptr_t> modules = GetProcessLibraries();
-
-	for (auto module : modules)
-		if (strcasestr(module.first, moduleName))
-			return module.second;
-
-	return 0;
-}
-
 /* Compare */
 bool Pattern::Compare(const unsigned char* pData, const unsigned char* bMask, const char* szMask) {
 	for (; *szMask; ++szMask, ++pData, ++bMask)
