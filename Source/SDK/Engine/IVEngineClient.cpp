2ae844905bc0c95ee01f56b12170ea71b44611a3
aphu <aphu@420blaze.it>
2017-01-29 14:10:46 -0500

    Update IVEngineClient.cpp

--- Source/SDK/Engine/IVEngineClient.cpp
+++ Source/SDK/Engine/IVEngineClient.cpp
@@ -8,3 +8,51 @@ int IVEngineClient::GetLocalPlayer() {
 	typedef int(*OriginalFn)(void*);
 	return GetVFunc<OriginalFn>(this, 12)(this);
 }
+
+/* ClientCmd_Unrestricted */
+void IVEngineClient::ClientCmd_Unrestricted(const char* szCmdString) {
+	typedef void(*OriginalFn)(void*, const char*);
+	return GetVFunc<OriginalFn>(this, 113)(this, szCmdString);	
+}
+
+/* ExecuteClientCmd */
+void IVEngineClient::ExecuteClientCmd(const char* szCmdString) {
+	typedef void(*OriginalFn)(void*, const char*);
+	return GetVFunc<OriginalFn>(this, 108)(this, szCmdString);
+}
+
+/* GetScreenSize */
+void IVEngineClient::GetScreenSize(int& iWidth, int& iHeight) {
+	typedef void(*OriginalFn)(void*, int&, int&);
+	return GetVFunc<OriginalFn>(this, 5)(this, iWidth, iHeight);
+}
+
+/* IsTakingScreenshot */
+bool IVEngineClient::IsTakingScreenshot() {
+	typedef bool(*OriginalFn)(void*);
+	return GetVFunc<OriginalFn>(this, 92)(this);
+}
+
+/* GetViewAngles */
+void IVEngineClient::GetViewAngles(Vector& vecAngle) {
+	typedef void(*OriginalFn)(void*, Vector&);
+	return GetVFunc<OriginalFn>(this, 18)(this, vecAngle);
+}
+
+/* SetViewAngles */
+void IVEngineClient::SetViewAngles(Vector& vecAngle) {
+	typedef void(*OriginalFn)(void*, Vector&);
+	return GetVFunc<OriginalFn>(this, 19)(this, vecAngle);
+}
+
+/* IsInGame */
+bool IVEngineClient::IsInGame() {
+	typedef bool(*OriginalFn)(void*);
+	return GetVFunc<OriginalFn>(this, 26)(this);
+}
+
+/* IsConnected */
+bool IVEngineClient::IsConnected() {
+	typedef bool(*OriginalFn)(void*);
+	return GetVFunc<OriginalFn>(this, 27)(this);
+}
